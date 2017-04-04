1a708ddfa58523604380784cfbabad2966190534
vxtools <kishore@vexata.com>
2017-04-04 00:26:11 -0700

    Update update_emulex_modprobe.sh

--- update_emulex_modprobe.sh
+++ update_emulex_modprobe.sh
@@ -5,7 +5,7 @@ SDIR=opt/emulex/lpfc
 MDIR=/etc/modprobe.d/
 
 # Disabling the irqbalance 
-# echo -e "status\nstop\ndisable\nstatus"|xargs -L1 -i systemctl {} irqbalance
+echo -e "status\nstop\ndisable\nstatus"|xargs -L1 -i systemctl {} irqbalance
 
 # Copying the lpfc startup files with recommended settings.
 cp -pr $SFILE $MDIR
